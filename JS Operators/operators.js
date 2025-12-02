c0ffb9b4f3d9270fb89c6fb36ab6d4f0ec110b2c
Rohithmaruthu <angurohith8@gmail.com>
2025-12-02 11:34:27 +0530

    Operators

---
+++ JS Operators/operators.js
@@ -0,0 +1,31 @@
+// JS Operators
+
+// 1. Arithmetic Operators - like +, -, *, /, %, **
+let a = 5;
+let b = 2;
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+console.log("Exponentiation:", a ** b);
+
+// 2. comparison Operators - like ==, ===, !=, !==, >, <, >=, <=
+let x = 10;
+console.log(x > 5); // true
+console.log(x < 5); // false
+
+// 3. Logical Operators - like &&, ||, !
+let isAdult = true;
+let hasID = false;
+
+if (isAdult && hasID) {
+    console.log("Access granted"); 
+} else {
+    console.log("Access denied"); 
+}
+
+// 4. Assignment Operators - like =, +=, -=, *=, /=, %=
+let num = 10;
+num += 5; // num = num + 5
+console.log("After += :", num);
